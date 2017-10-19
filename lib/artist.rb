10ef576b4e9a13727663f5a45e0716cace909206
Adam Fudge <coolfudge@gmail.com>
2017-10-19 21:21:16 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+class Artist
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all = []
+  end
+
+  def find_or_create_by_name(artist_name)
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def songs
+    @songs
+  end
+
+  def save
+    @@all << self
+    @@all.uniq
+  end
+
+  def self.all
+    @@all
+  end
+end
