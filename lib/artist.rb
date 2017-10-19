a4a69838e8b9fd7579c97fd48625b24289e51e5b
Adam Fudge <coolfudge@gmail.com>
2017-10-19 21:59:59 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,7 +8,6 @@ class Artist
   end
 
   def self.find_or_create_by_name(artist_name)
-    #search for artist_name
     artist = @@all.find { |artist| artist_name == artist.name}
 
     if artist == nil
