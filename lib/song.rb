e3e6d2bdfc1b2aad6b2d0cfb8ddcf0c51447d092
Adam Fudge <coolfudge@gmail.com>
2017-10-20 00:03:55 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,6 +9,7 @@ class Song
     parsed_filename = filename.split(" - ")
     new_song = Song.new(parsed_filename[1])
     new_song.artist = Artist.find_or_create_by_name(parsed_filename[0])
+    new_song.artist.add_song(parsed_filename[1])
     new_song
   end
 
